b62d575388787a86393115592f0bc4391d725b5d
everzet <ever.zet@gmail.com>
2014-12-08 10:58:04 +0000

    Append definition snippets for the first scenario

--- features/bootstrap/BasketContext.php
+++ features/bootstrap/BasketContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 
@@ -17,5 +18,29 @@ class BasketContext implements Context, SnippetAcceptingContext
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given there is a product with SKU :arg1 and a cost of £:arg2 in the catalogue
+     */
+    public function thereIsAProductWithSkuAndACostOfPsInTheCatalogue($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add the product with SKU :arg1 from the catalogue to my basket
+     */
+    public function iAddTheProductWithSkuFromTheCatalogueToMyBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the total price of my basket should be £:arg1
+     */
+    public function theTotalPriceOfMyBasketShouldBePs($arg1)
+    {
+        throw new PendingException();
     }
 }
