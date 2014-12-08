e58800a99aa54c4f9982fafddb0d6760a642bddc
everzet <ever.zet@gmail.com>
2014-12-08 10:55:03 +0000

    Define and initialize the domain test suite
    1. Create the suite configuration in `behat.yml`
    2. Run `bin/behat --init`

---
+++ features/bootstrap/BasketContext.php
@@ -0,0 +1,21 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class BasketContext implements Context, SnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+}
