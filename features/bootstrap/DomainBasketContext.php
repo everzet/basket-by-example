c15ecbb69b309fc78a289e34d8f192296754fbbc
everzet <ever.zet@gmail.com>
2016-11-10 02:24:10 +0000

    Generate DomainBasketContext to not waste time

---
+++ features/bootstrap/DomainBasketContext.php
@@ -0,0 +1,15 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Basket\Cost;
+use Basket\Product;
+use Basket\Sku;
+use Basket\Basket;
+use Money\Money;
+
+class DomainBasketContext implements Context
+{
+    public function __construct()
+    {
+    }
+}
